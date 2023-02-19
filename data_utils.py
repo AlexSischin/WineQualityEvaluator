7e7503921cf679e8f91aa70f5b682afdd0c6d167
Alex Shishkin <alexsischin@gmail.com>
2023-02-19 02:15:00 +0200

    Routine for comparing polynomial degree; Using random seed, so we don't use test data for training or development purposes

--- data_utils.py
+++ data_utils.py
@@ -2,10 +2,10 @@ import pandas as pd
 from pandas import DataFrame, Series
 
 
-def read_data(filename: str, columns: list[str], sep=','):
+def read_data(filename: str, columns: list[str], sep=',', random_state=None):
     df = pd.read_csv(filename, sep=sep)
     df = df[columns]
-    df = df.sample(frac=1)
+    df = df.sample(frac=1, random_state=random_state)
     df.reset_index(drop=True, inplace=True)
     return df
 
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import matplotlib.pyplot as plt
 import pandas as pd
-from pandas import DataFrame
+from pandas import DataFrame, Series
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, PolynomialFeatures
 
 from columns import *
 from data_utils import read_data, split_x_y
@@ -47,23 +50,76 @@ def separate_9th_grade_wine(df: DataFrame):
     return wine9_df, other_wine
 
 
+def get_train_dev_test_white_wine():
+    all_wine = read_data(white_wine_file, white_wine_cols, sep=';', random_state=123)
+    wine9, wine_not9 = separate_9th_grade_wine(all_wine)
+    w_wine_train_df, wine_dev_test_not9 = train_test_split(wine_not9, test_size=0.6, shuffle=False)
+    w_wine_not9_dev_df, w_wine_not9_test_df = train_test_split(wine_dev_test_not9, test_size=0.5, shuffle=False)
+    wine9_dev, wine9_test = train_test_split(wine9, test_size=0.5, shuffle=False)
+    w_wine_dev_df = pd.concat([w_wine_not9_dev_df, wine9_dev], axis=0)
+    w_wine_test_df = pd.concat([w_wine_not9_test_df, wine9_test], axis=0)
+    return w_wine_train_df, w_wine_dev_df, w_wine_test_df
+
+
+def compare_poly_degrees(train_x: DataFrame, train_y: Series, dev_x: DataFrame, dev_y: Series):
+    train_x = train_x.to_numpy()
+    train_y = train_y.to_numpy()
+    dev_x = dev_x.to_numpy()
+    dev_y = dev_y.to_numpy()
+
+    degree_list = list(range(1, 4))
+    feature_number_list = []
+    train_mse_list = []
+    dev_mse_list = []
+
+    for d in degree_list:
+        # Add polynomial features
+        poly_features = PolynomialFeatures(degree=d, include_bias=False)
+        train_x = poly_features.fit_transform(train_x)
+        dev_x = poly_features.transform(dev_x)
+
+        # Scale features
+        w_wine_scaler = StandardScaler()
+        train_x = w_wine_scaler.fit_transform(train_x)
+        dev_x = w_wine_scaler.transform(dev_x)
+
+        # Train
+        model = LinearRegression()
+        model.fit(train_x, train_y)
+
+        # Test
+        train_yhat = model.predict(train_x)
+        dev_yhat = model.predict(dev_x)
+        train_mse = mean_squared_error(train_y, train_yhat)
+        dev_mse = mean_squared_error(dev_y, dev_yhat)
+
+        # Save
+        feature_number_list.append(poly_features.n_output_features_)
+        train_mse_list.append(train_mse)
+        dev_mse_list.append(dev_mse)
+
+    fig, (mse_ax, feature_ax) = plt.subplots(1, 2)
+    mse_ax.set_title('MSE VS polynomial degree')
+    mse_ax.set_xlabel('Polynomial degree')
+    mse_ax.set_ylabel('MSE')
+    mse_ax.plot(degree_list, train_mse_list, label='Train MSE', marker='.')
+    mse_ax.plot(degree_list, dev_mse_list, label='Dev MSE', marker='.')
+    mse_ax.legend()
+    feature_ax.set_title('Feature number VS polynomial degree')
+    feature_ax.set_xlabel('Polynomial degree')
+    feature_ax.set_ylabel('Feature number')
+    feature_ax.plot(degree_list, feature_number_list, marker='.')
+    plt.show()
+
+
 def main():
-    _wine = read_data(white_wine_file, white_wine_cols, sep=';')
-    _wine9, _wine_other = separate_9th_grade_wine(_wine)
-    w_wine_train_df, _wine_other_dev_test = train_test_split(_wine_other, test_size=0.6, shuffle=False)
-    _wine_dev_test = pd.concat([_wine_other_dev_test, _wine9], axis=0)
-    w_wine_dev_df, w_wine_test_df = train_test_split(_wine_dev_test, test_size=0.5, shuffle=True)
-    del _wine, _wine9, _wine_other_dev_test, _wine_dev_test
+    w_wine_train_df, w_wine_dev_df, w_wine_test_df = get_train_dev_test_white_wine()
 
     w_wine_train_x, w_wine_train_y = split_x_y(w_wine_train_df, COL_QUALITY)
     w_wine_dev_x, w_wine_dev_y = split_x_y(w_wine_dev_df, COL_QUALITY)
     w_wine_test_x, w_wine_test_y = split_x_y(w_wine_test_df, COL_QUALITY)
 
-    w_wine_scaler = StandardScaler().fit(w_wine_train_x)
-
-    w_wine_train_x = w_wine_scaler.transform(w_wine_train_x)
-    w_wine_dev_x = w_wine_scaler.transform(w_wine_dev_x)
-    w_wine_test_x = w_wine_scaler.transform(w_wine_test_x)
+    compare_poly_degrees(w_wine_train_x, w_wine_train_y, w_wine_dev_x, w_wine_dev_y)
 
 
 if __name__ == '__main__':
